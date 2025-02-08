767ee8d350c80a4882cba09e8bdf790bb89df9b8
Dima <dimaisay1109@gmail.com>
2025-02-08 16:07:41 +0200

    Task 4 is done

---
+++ task_4/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "../task_3/eratosphen.cpp"
+#include <vector>
+#include <string>
+#include "../functions.cpp"
+#include <stdexcept>
+
+int main()
+{
+    int number;
+    std::string input;
+    int* array;
+
+    while(true)
+    {
+        std::cout << "Enter a number: ";
+
+        try
+        {
+            std::getline(std::cin, input);
+            number = check_input(input);
+            array = eratosphen(number);
+        }
+        catch (const char* error)
+        {
+            std::cerr << error << std::endl;
+            continue;
+        }
+        catch (const std::invalid_argument& ai)
+        {
+            std::cerr << "Invalid argument: " << ai.what() << std::endl;
+            continue;
+        }
+        
+        std::vector<int> result;
+        
+        int i = 0;
+        while(i < number - 1)
+        {
+            if (!array[i])
+            {
+                i++;
+                continue;
+            }
+            else if (number % array[i] == 0)
+            {
+                result.push_back(array[i]);
+                number /= array[i];
+            }
+            else i++;
+        }
+
+        for (auto it = result.cbegin(); it != result.cend(); it++)
+            std::cout << *it << std::endl;
+    }
+}
